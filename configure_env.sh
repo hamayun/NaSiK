9e92c770af80315c52483524392940900c4e9ae5
hamayun <mian-muhammad.hamayun@imag.fr>
2010-09-03 11:48:48 +0200

    Makefile.rules for arm966, App+OS compilable using this config; but simulation on Soclib/SystemCASS is a pending work

--- configure_env.sh
+++ configure_env.sh
@@ -5,10 +5,12 @@ APP_DIR=$PROJ_TOPDIR/examples/mjpegmthr
 
 export LIBTA_HOME=/opt/libs/libta
 export SYSTEMC=/opt/libs/systemc-2.2.0
+export CONFIGURATION=native
 
 echo "LIBTA_HOME = $LIBTA_HOME"
 echo "SYSTEMC = $SYSTEMC"
 echo "APPLICATION = $APP_DIR"
+echo "CONFIGURATION = $CONFIGURATION"
 export LD_LIBRARY_PATH=$LIBTA_HOME/lib:$LD_LIBRARY_PATH
 
 echo "[Setting up Software platform environment]"
@@ -24,7 +26,7 @@ export PATH=/opt:$PATH
 
 # Now move to Application Dir
 cd $APP_DIR/sw
-source install.sh native
+source install.sh $CONFIGURATION
 
 cd $PROJ_TOPDIR
 
