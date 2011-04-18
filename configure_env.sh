f7aee1c069bb16a9eff74a877e5ed435cf94418b
Mian M. Hamayun <mian-muhammad.hamayun@imag.fr>
2011-04-18 20:18:58 +0200

    Added the stringsearch Application to NativeMiBench

--- configure_env.sh
+++ configure_env.sh
@@ -25,7 +25,8 @@ export APES_PATH=$APES_PATH:$APES_EXTRA_COMPS
 #APP_NAME=CRC32
 #APP_NAME=bitcount
 #APP_NAME=cjpeg
-APP_NAME=djpeg
+#APP_NAME=djpeg
+APP_NAME=stringsearch
 echo "[Setting-up Software Application Environment ($APP_NAME)]"
 APP_DIR=$(find $PROJ_TOPDIR/examples -name "$APP_NAME")
 cd $APP_DIR/sw
--- examples/NativeMiBench/consumer/djpeg/sw/install.sh
+++ examples/NativeMiBench/consumer/djpeg/sw/install.sh
@@ -7,13 +7,13 @@ CUSTOM_INCLUDES="-I${SECONDARY_TOOLCHAIN}/i386-sls-dnaos/include/ -I${SECONDARY_
 #export APES_SPLIT_CC=""
 export APES_CC1="llvm-gcc"
 export APES_CC2="llc"
-export APES_CC1_FLAGS="-pipe -Wall -Wno-format -std=c99 -O4 -U__linux__ --emit-llvm $CUSTOM_INCLUDES"
+export APES_CC1_FLAGS="-pipe -Wall -Wno-format -std=c99 -O3 -U__linux__ --emit-llvm $CUSTOM_INCLUDES"
 export APES_CC2_FLAGS="-annotate=arm" 
 # "-march=arm --print-dual-cfg --print-annotated-cfg"
 
 # On Failure Use the default APES_COMPILER
 export APES_COMPILER="i386-sls-dnaos-gcc"
-export APES_CC_FLAGS="-Wall -Wno-format -std=c99 -O4 $CUSTOM_INCLUDES"
+export APES_CC_FLAGS="-Wall -Wno-format -std=c99 -O3 $CUSTOM_INCLUDES"
 
 export APES_ASSEMBLER="i386-sls-dnaos-as"
 
