b72ceb878392481fdfcfa2a2b73cb7e036447df4
Mian M. Hamayun <mian-muhammad.hamayun@imag.fr>
2011-07-06 22:30:59 +0200

    Added support for ARM Processor to MiBench Applications. - Using dedicated interface.xmi and ldscript files. - Three Applications (Patricia, CRC32 and BitCount) have Issues with Floating Point printf/fprintf. - Added Dedicated Configure Scripts for ARM and KVM.

--- configure_env_arm.sh
+++ configure_env_arm.sh
@@ -1,14 +1,7 @@
-echo "[Setting-up Hardware Platform Environment (SystemC, LIBKVM)]"
 export NASIK_HOME=$PWD
-export SYSTEMC=/opt/libs/systemc-2.2.0
-export LIBKVM_HOME=${NASIK_HOME}/hw/kvm-85
-export LIBSOCKVM_HOME=${NASIK_HOME}/hw/soc_kvm
-export LIBKVM_PREFIX=/opt/libs/libkvm
 
 echo "[Setting-up Software Platform Environment (APES+Toolchains)]"
-export PRIMARY_TOOLCHAIN=/opt/toolchains/llvm-2.8
 export SECONDARY_TOOLCHAIN=/opt/toolchains/apes-arm
-#export SECONDARY_TOOLCHAIN=/opt/toolchains/apes-i386-annotated
 export APES_ROOT=/opt/Apes
 export APES_EXTRA_COMPS=$NASIK_HOME/sw/apes-components
 source $APES_ROOT/install.sh
@@ -16,9 +9,9 @@ export APES_PATH=$APES_PATH:$APES_EXTRA_COMPS
 
 #APPLICATION=kvmParallelMjpeg
 #APPLICATION=susan
-APPLICATION=qsort
+#APPLICATION=qsort
 #APPLICATION=dijkstra
-#APPLICATION=patricia
+APPLICATION=patricia
 #APPLICATION=blowfish
 #APPLICATION=rijndael
 #APPLICATION=sha
@@ -30,9 +23,6 @@ APPLICATION=qsort
 #APPLICATION=kvmPi
 export APPLICATION
 export APP_DIR=$(find $NASIK_HOME/examples/applications -name "$APPLICATION")
-
-export PLATFORM=tuzki
-export PFORM_DIR=$NASIK_HOME/examples/platforms/$PLATFORM
 
 echo "[Setting-up Software Application Environment ($APPLICATION)]"
 cd $NASIK_HOME/examples/applications
@@ -53,35 +43,18 @@ ln -sf interface.xmi.arm interface.xmi
 # for tty_terms
 export PATH=$NASIK_HOME:$PATH
 export PATH=${SECONDARY_TOOLCHAIN}/bin:$PATH
-export PATH=${PRIMARY_TOOLCHAIN}/bin:$PATH
-export LD_LIBRARY_PATH=${LIBKVM_PREFIX}/lib:$LD_LIBRARY_PATH
 
 cd $NASIK_HOME
 
 echo "======================= GENERAL ENVIRONMENT SETTINGS ========================="
-echo "SYSTEMC                 = $SYSTEMC"
-echo "LIBKVM_HOME             = $LIBKVM_HOME"
-echo "LIBKVM_PREFIX           = $LIBKVM_PREFIX"
 echo "APPLICATION             = $APP_DIR"
-echo "PLATFORM                = $PFORM_DIR"
 echo "APES_ROOT               = $APES_ROOT"
 echo "APES EXTRA COMPONENTS   = $APES_EXTRA_COMPS"
 echo "APES_PATH               = $APES_PATH"
 
-if [ ${!APES_SPLIT_CC[@]} ]; then
-	echo "======================= SPLIT COMPILATION TOOLCHAIN =========================="
-	echo "PRIMARY_TOOLCHAIN       = $PRIMARY_TOOLCHAIN"
-	echo "SECONDARY_TOOLCHAIN     = $SECONDARY_TOOLCHAIN"
-	echo "APES_CC1                = $APES_CC1"
-	echo "APES_CC2                = $APES_CC2"
-	echo "APES_CC1_FLAGS          = $APES_CC1_FLAGS"
-	echo "APES_CC2_FLAGS          = $APES_CC2_FLAGS"
-else 
-	echo "======================= APES COMPILATION TOOLCHAIN ==========================="
-	echo "APES_COMPILER           = $APES_COMPILER"
-	echo "APES_CC_FLAGS           = $APES_CC_FLAGS"
-fi
-
+echo "======================= APES COMPILATION TOOLCHAIN ==========================="
+echo "APES_COMPILER           = $APES_COMPILER"
+echo "APES_CC_FLAGS           = $APES_CC_FLAGS"
 echo "APES_CC_OPTIMIZATIONS   = $APES_CC_OPTIMIZATIONS"
 echo "APES_LINKER             = $APES_LINKER"
 echo "APES_LINKER_FLAGS       = $APES_LINKER_FLAGS"
---
+++ examples/applications/debug_app_arm.sh
@@ -0,0 +1,1 @@
+/opt/toolchains/apes-arm/bin/arm-sls-dnaos-gdb -x $NASIK_HOME/examples/applications/gdb_commands.in "$@"
