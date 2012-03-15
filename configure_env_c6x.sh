4cf871f3ff6d7e846344577af5692ec55eadc3ed
Mian M. Hamayun <mian-muhammad.hamayun@imag.fr>
2012-03-15 11:08:37 +0100

    Tested with Recursive Fibonacci Example

--- configure_env_c6x.sh
+++ configure_env_c6x.sh
@@ -13,7 +13,8 @@ export BAREMETAL_TOOLCHAIN=/opt/toolchains/i386-pc-baremetal
 export TARGET_BIN_WRITER=/home/hamayun/examples/binary-file-writer
 
 export CCS_WORKSPACE_PATH=/home/hamayun/workspace_ccs
-export CCS_EXAMPLE_NAME=matmult
+#export CCS_EXAMPLE_NAME=matmult
+export CCS_EXAMPLE_NAME=fibonacci
 export CCS_EXAMPLE_BUILD=Debug
 export CCS_EXAMPLE_OUTFILE=${CCS_EXAMPLE_NAME}.out
 export CCS_EXAMPLE_PATH=${CCS_WORKSPACE_PATH}/${CCS_EXAMPLE_NAME}/${CCS_EXAMPLE_BUILD}/${CCS_EXAMPLE_OUTFILE}
